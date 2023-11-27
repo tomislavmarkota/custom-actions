36d2e8a0e31b6ff55444ba0fcf2f83087b66f900
Tomislav <124046839+markotatomislav@users.noreply.github.com>
2023-11-27 23:03:13 +0100

    custom js code

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,13 @@
+// https://docs.github.com/en/actions/creating-actions/creating-a-javascript-action
+// @actions/core
+// @actions/github
+// @actions/exec
+import core from "@actions/core";
+import github from "@actions/github";
+import exec from "@actions/exec";
+
+function run() {
+  core.notice("hello from my custom js action");
+}
+
+run();
